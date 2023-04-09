9c6e63f36131f955ea63fe82343f2dd94aefa8b8
Bishopaul <monitepaul@gmail.com>
2023-04-09 05:07:47 -0700

    My seventh commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,39 +7,39 @@
  */
 void times_table(void)
 {
-        int i, j, k;
+	int i, j, k;
 
-        for (i = 0; i < 10; i++)
-        {
-                for (j = 0; j < 10; j++)
-                {
-                        k = i * j;
-                        if (k < 10)
-                        {
-                                if (j != 0)
-                                {
-                                        _putchar(' ');
-                                        _putchar(' ');
-                                }
-                                _putchar(k + '0');
-                        }
-                        else if (k < 100)
-                        {
-                                _putchar(' ');
-                                _putchar(k / 10 + '0');
-                                _putchar(k % 10 + '0');
-                        }
-                        else
-                        {
-                                _putchar(k / 100 + '0');
-                                _putchar((k / 10) % 10 + '0');
-                                _putchar(k % 10 + '0');
-                        }
-                        if (j != 9)
-                        {
-                                _putchar(',');
-                        }
-                }
-                _putchar('\n');
-        }
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = i * j;
+			if (k < 10)
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(k + '0');
+			}
+			else if (k < 100)
+			{
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+			else
+			{
+				_putchar(k / 100 + '0');
+				_putchar((k / 10) % 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
 }
