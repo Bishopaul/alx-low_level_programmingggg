a0279ed73e115b9cdd325cd9b23d8ab68d739e24
Bishopaul <monitepaul@gmail.com>
2023-04-10 01:26:11 -0700

    Argument

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Prints it's name followed by a new line
+ *
+ * @argc: Argument count
+ * @argv: Argument
+ * Return: 0
+ */
+
 int main(int argc, char **argv)
 {
 	(void)argc;
